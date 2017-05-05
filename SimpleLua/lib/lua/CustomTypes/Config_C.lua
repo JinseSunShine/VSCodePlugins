1584b3eebb1088a1ffdc676ea3f1562d70324163
chenliang3 <chenliang3@kingsoft.com>
2017-05-05 17:26:42 +0800

    优化

--- SimpleLua/lib/lua/CustomTypes/Config_C.lua
+++
@@ -1,6 +0,0 @@
-local CommonUtils = require "CommonUtils"
-
-return function (VarAst)
-    local VarValue = VarAst.localdefinition and VarAst.localdefinition.value
-    CommonUtils.SetStaticTable(VarValue, 3)
-end
